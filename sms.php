f5491b64680adac96f8eab5dc593cb38af2ebb59
dallaze <143823308+dallaze@users.noreply.github.com>
2023-09-01 17:06:16 +0200

    Add files via upload

---
+++ sms.php
@@ -0,0 +1,10 @@
+<?php
+$url = $_ENV["ttps://041d6327-e993-45ab-af63-a93731d8b1ba:FHsrl_va-YF5RDA0yNRhrg@api.blower.io"] . "/messages"; $data = array('to' => '+61422123456', 'message' => 'Hello from Blower.io'); 
+$ch = curl_init($url); 
+
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
